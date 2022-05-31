185929501d10f2eaa1b904d65e7d89b914ec1ec8
Lubor Budaj <l.budaj@student.rug.nl>
2022-05-31 13:43:21 +0200

    updated modus ponens and its proof

--- propositional.v
+++ propositional.v
@@ -55,22 +55,33 @@ Inductive ax_pl : Form -> Prop :=
   | a_2 : forall x y : Form, ax_pl (Impl x (Impl y x))
   | a_3 : forall x y z : Form, ax_pl (Impl (Impl x (Impl y z)) (Impl (Impl x y) (Impl x z)))
   | a_4 : forall x y : Form, ax_pl (Impl (Impl (Neg x) (Neg y)) (Impl y x))
-  | a_5 (x y z : Form) : ax_pl (Impl (Conj (Impl x y) x) y).
-
-(*
-
-Inductive ax_pl : Form -> Prop :=
-  | a_1 (x : Form) : ax_pl (Impl x x)
-  | a_2 (x y : Form) : ax_pl (Impl x (Impl y x))
-  | a_3 (x y z : Form) : ax_pl (Impl (Impl x (Impl y z)) (Impl (Impl x y) (Impl x z)))
-  | a_4 (x y : Form) : ax_pl (Impl (Impl (Neg x) (Neg y)) (Impl y x))
-  | a_5 (x y z : Form) : ax_pl (Impl (Conj (Impl x y) x) y).
-
-*)
+  | mp : forall x y : Form, ax_pl (Impl x y) -> ax_pl x -> ax_pl y.
+(*  | mp (x y z : Form) : ax_pl (Impl x y) -> ax_pl x -> ax_pl y. *)
 
 Check ax_pl.
 Check ax_pl_ind.
 Check ax_pl_sind.
+
+Lemma excluded_middle : forall P : bool,
+  Is_true (or P (neg P)).
+Proof.
+  intros P. unfold Is_true, or, neg. destruct P; reflexivity.
+Qed.
+
+Lemma mp1 : forall x y : Prop, (x /\ (x -> y)) -> y.
+Proof.
+  intros x y H. destruct H. pose proof (H0 H) as H1. apply H1.
+Qed.
+
+Lemma mp2 : forall x y : Prop, (x -> y) -> x -> y.
+Proof.
+  intros x y H0 H1. pose proof (H0 H1) as H2. apply H2.
+Qed.
+
+Lemma mp3 : forall x y : Prop, (x /\ (x -> y)) -> y = (x -> y) -> x -> y.
+Proof.
+  intros x y H0 H1 H2. destruct H0. pose proof (H0 H). apply H3.
+Qed.
 
 Lemma ax_pl_sound (x : Form) :
   ax_pl x -> forall i, interpretP x i.
@@ -88,10 +99,22 @@ Proof.
   - intros i. unfold interpretP, Is_true. simpl. unfold implies, or, neg. destruct (interpret x i).
     + destruct (interpret y i); reflexivity.
     + destruct (interpret y i); reflexivity.
-  - intros i. unfold interpretP, Is_true. simpl. unfold implies, and, or, neg. destruct (interpret x i).
-    + destruct (interpret y i); reflexivity.
+  - intros i. unfold interpretP, Is_true. pose proof (IHax_pl1 i). pose proof (IHax_pl2 i). 
+    unfold interpretP, Is_true in H1, H2. simpl in H1. unfold implies, or, neg in H1. destruct (interpret y i).
     + reflexivity.
+    + destruct (interpret x i).
+      * apply H1.
+      * apply H2.
 Qed.
+
+(*
+destruct H eqn:EE. destruct H0 eqn:EEE.
+unfold interpretP, Is_true. unfold interpretP, Is_true in IHax_pl1, IHax_pl2. simpl in IHax_pl1.
+     assert (H1: Is_true (or (interpret x i) (neg (interpret x i)))). apply excluded_middle. unfold Is_true, or, neg in H1. unfold implies in IHax_pl1.
+
+destruct (interpret y i).
+    + reflexivity.
+*)
 
 Declare Scope bool_scope.
 Delimit Scope bool_scope with B.
